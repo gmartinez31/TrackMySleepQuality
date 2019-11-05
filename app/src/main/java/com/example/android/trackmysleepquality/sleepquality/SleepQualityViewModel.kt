03d0a61d2ca9b2edca5af9f125968a5f67e06e89
gmartinez31 <bhgustavo13@yahoo.com>
2019-11-04 22:00:31 -0700

    vm and factory for sleepquality fragment

--- app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityViewModel.kt
@@ -15,3 +15,42 @@
  */
 
 package com.example.android.trackmysleepquality.sleepquality
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+import com.example.android.trackmysleepquality.database.SleepNight
+import kotlinx.coroutines.*
+
+class SleepQualityViewModel(
+        private val sleepNightKey: Long = 0L,
+        val databaseDao: SleepDatabaseDao ) : ViewModel() {
+
+    private val vmJob = Job()
+    private val uiScope = CoroutineScope(Dispatchers.Main + vmJob)
+
+    private val _navigateToSleepTrackerFragment = MutableLiveData<Boolean?>()
+    val navigateToSleepTrackerFragment: LiveData<Boolean?>
+        get() = _navigateToSleepTrackerFragment
+
+    override fun onCleared() {
+        super.onCleared()
+        vmJob.cancel()
+    }
+
+    fun doneNavigating() {
+        _navigateToSleepTrackerFragment.value = null
+    }
+
+    fun onSetSleepQuality( sleepQuality: Int ) {
+        uiScope.launch {
+            withContext(Dispatchers.IO) {
+                val tonight = databaseDao.get(sleepNightKey) ?: return@withContext
+                tonight.sleepQuality = sleepQuality
+                databaseDao.update(tonight)
+            }
+            _navigateToSleepTrackerFragment.value = true
+        }
+    }
+}
